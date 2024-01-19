9a5cfbb6aa81274139f77d8a12a5f8ae3200e55b
DanaSalim <danasalim@gmail.com>
2024-01-19 17:31:48 +0600

    Implement pages: About, Contacts. Adapt them

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,3 +1,46 @@
+import { Flex, Image, Space, Typography} from 'antd';
+import {GithubOutlined} from '@ant-design/icons';
+import { useWindowSize } from '@uidotdev/usehooks';
+
+import { StyledCard } from '../../components/StyledCard';
+import AvatarImage from '../../assets/img/about_avatar.jpg';
+import styles from './index.module.scss';
+
+
 export const About = () => {
-  return <div>About</div>;
-};
+    const {width} = useWindowSize();
+
+
+    return (
+        <Flex vertical gap={20} className={styles.about}>
+            <StyledCard >
+                <Flex vertical={width < 1024} gap={50} justify='space-between' align='center'>
+                    <Image src={AvatarImage} rootClassName={styles.avatar}/>
+                    <Flex vertical gap='small' align='flex-start' style={{width: '100%'}}>
+                        <Typography.Title style={{fontSize: width < 768 ? 30 : 38}}>Дана Галымжанкызы</Typography.Title>
+                        <Flex vertical>
+                            <Typography.Text style={{fontSize: 24}}>Навыки:</Typography.Text>
+                            <Flex vertical style={{marginLeft: 10}}>
+                                <Typography.Text style={{fontSize: 18}}>-Коммуникабельность</Typography.Text>
+                                <Typography.Text style={{fontSize: 18}}>-Быстрообучаемость</Typography.Text>
+                                <Typography.Text style={{fontSize: 18}}>-Ответственность</Typography.Text>
+                                <Typography.Text style={{fontSize: 18}}>-Отзывчивость</Typography.Text>
+                            </Flex>
+                        </Flex>
+                        <Flex vertical>
+                            <Typography.Text style={{fontSize: 24}}>Стек используемый в данном проекте:</Typography.Text>
+                            <Flex vertical style={{marginLeft: 10}}>
+                                <Typography.Text style={{fontSize: 18}}>-React</Typography.Text>
+                                <Typography.Text style={{fontSize: 18}}>-Vite</Typography.Text>
+                                <Typography.Text style={{fontSize: 18}}>-Ant Design</Typography.Text>
+                                <Typography.Text style={{fontSize: 18}}>-React Formik</Typography.Text>
+                                <Typography.Text style={{fontSize: 18}}>-React Hook Form</Typography.Text>
+                                <Typography.Text style={{fontSize: 18}}>-Yup</Typography.Text>
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                </Flex>
+            </StyledCard>
+        </Flex>
+    );
+}
